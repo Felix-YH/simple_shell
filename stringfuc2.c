5baa9d2c889080084c3f96b94446ea2cb60c29e2
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-05-12 18:50:48 +0100

    Update stringfuc2.c

--- stringfuc2.c
+++ stringfuc2.c
@@ -51,3 +51,46 @@ char **strbrk(char *line, char c)
         array[j] = NULL;
         return (array);
 }
+
+
+/**
+  * stringer - a helper function to the string breaker
+  * @line: a string pointer
+  * @i: the index of the start of the string
+  * @str: a string pointer to the location
+  * @c: a character which specifys where to end the string
+  * Return: returns a string pointer
+  */
+
+char *stringer(char *line, int i, char *str, char c)
+{
+        int k = 0;
+        char *tmp;
+
+        str = malloc(64);
+        if (str == NULL)
+        {
+                perror("Couldn't Allocate");
+                return (NULL);
+        }
+        while (line[i] != c && line[i] != '\0')
+        {
+                str[k] = line[i++];
+                k++;
+                if (k > 60)
+                {
+                        tmp = realloc(str, (k + 8));
+                        if (!tmp)
+                        {
+                                perror("Error");
+                                return (NULL);
+                        }
+                        else
+                                str = tmp;
+                }
+        }
+
+        str[k] = '\0';
+
+        return (str);
+}
