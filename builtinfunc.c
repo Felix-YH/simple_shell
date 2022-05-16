8b33f58222d5ade23104b793fa8916fe7a7d9051
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-05-16 15:07:31 +0100

    Update builtinfunc.c

--- builtinfunc.c
+++ builtinfunc.c
@@ -39,3 +39,38 @@ int builtincom(char **line)
         free(line);
         return (0);
 }
+
+
+/**
+ * echorr - a function which echo back any text
+ * @line: an array of command and arguments
+ *
+ * Return: returns 1 in success and -1 if it fails
+ */
+ 
+int echorr(char *line[])
+{
+        int status;
+        pid_t waiter, child;
+
+        child = fork();
+        if (child == -1)
+        {
+                perror("Forking");
+        }
+        if (child == 0)
+        {
+                execve("/bin/echo", line, NULL);
+        }
+        else
+        {
+                waiter = wait(&status);
+                if (waiter == -1)
+                {
+                        perror("Waiting");
+                }
+        }
+        free(line);
+
+        return (1);
+}
