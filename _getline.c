f60d96a0d440a41f62a516969939b965ebcce90a
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-05-16 14:47:48 +0100

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -36,3 +36,41 @@ char _getchar(void)
 
         return (c);
 }
+
+
+/**
+ * _getline - a function to read a line from the standard input
+ * @line: a pointer to a pointer of location to save the string
+ * @len: the size of the characters read
+ *
+ * Return: returns the size of the read string
+ */
+
+int _getline(char **line, size_t *len)
+{
+        size_t limit = 25;
+        char *tmp;
+
+        line[0] = malloc(25);
+        if (!line[0])
+                return (-1);
+        *len = 0;
+
+        while (line[0][*len - 1] != '\n')
+        {
+                line[0][*len] = _getchar();
+                *len += 1;
+
+                if (*len > (limit - 3))
+                {
+                        tmp = realloc(line[0], limit + 10);
+                        if (tmp)
+                                line[0] = tmp;
+                        else
+                                perror("Reallocation");
+                        limit += 10;
+                }
+        }
+
+        return (*len);
+}
