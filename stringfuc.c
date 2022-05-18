79ace9dfd0b30110eaa2ab35a9d2abff5de3ee16
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-05-18 09:46:40 +0100

    Update stringfuc.c

--- stringfuc.c
+++ stringfuc.c
@@ -1,104 +1,158 @@
-#include "shell.h"
+#include "simishell.h"
 
 /**
- * getstr - function to read a string from  the cli
- * @str: a string pointer
- *
- * Return: return 1 in success and -1 in failure
- */
+* getstr - a function to read a string from  the cli
+* @str: a string pointer
+*
+* Return: return 1 in success and -1 in failure
+*/
 
 int getstr(char *str)
 {
-	char **line;
-	size_t siz = 0;
-
-	signal(SIGINT, sigintHandler);
-	line = malloc(24);
-	if (!line)
-	{
-		return (-1);
-	}
-
-	if ((getline(line, &siz, stdin)) == -1)
-	{
-		return (-1);
-	}
-
-	line[0][siz] = '\0';
-
-	strcopy(line[0], str);
-
-	if (line[0][siz - 2] == '\\')
-	{
-		do {
-			str[strleng(str) - 1] = ' ';
-			siz = 0;
-                        printprompt(1);
-
-                        if ((getline(line, &siz, stdin)) == -1)
-                        {
-                                return (-1);
-                        }
-                        line[0][siz] = '\0';
-
-                        strmix(line[0], str);
-                } while (line[0][siz - 2] == '\\');
-        }
-
-        free(line);
-        return (1);
-}
-
-/**
- * strcopy - a function to copy to strings
- * @src: the string to be copied
- * @dest: the destination of the copied string
- *
- * Return: returns nothing (void)
- */
-        void strcopy(char *src, char *dest)
+char **line;
+size_t siz = 0;
+signal(SIGINT, sigintHandler);
+line = malloc(24);
+if (!line)
 {
-        int i = 0;
-        int j = 0;
-        if (!dest)
-        {
-                j = 0;
-        }
-        while (src[i] != '\0' && src[i] != '\n')
-        {
-                dest[j++] = src[i++];
-        }
-        dest[j] = '\0';
+return (-1);
 }
 
+if ((getline(line, &siz, stdin)) == -1)
+{
+return (-1);
+}
+
+line[0][siz] = '\0';
+
+strcopy(line[0], str);
+
+if (line[0][siz - 2] == '\\')
+{
+do {
+str[strleng(str) - 1] = ' ';
+siz = 0;
+printprompt(1);
+
+if ((getline(line, &siz, stdin)) == -1)
+{
+return (-1);
+}
+line[0][siz] = '\0';
+
+strmix(line[0], str);
+} while (line[0][siz - 2] == '\\');
+}
+
+free(line);
+return (1);
+}
 
 /**
-* strmix - a function to copy to strings and concatenate if
-*        the destination have a string already.
+* strcopy - a function to copy to strings
 * @src: the string to be copied
 * @dest: the destination of the copied string
 *
 * Return: returns nothing (void)
 */
-void strmix(char *src, char *dest)
+
+void strcopy(char *src, char *dest)
 {
-        int i = 0;
-        int j;
-        if (!dest)
-        {
-                j = 0;
-        }
-        else
-        {
-                j = strleng(dest);
-        }
-        while (src[i] != '\0' && src[i] != '\n')
-        {
-                dest[j++] = src[i++];
-        }
-        dest[j] = '\0';
+int i = 0;
+int j = 0;
+
+if (!dest)
+{
+j = 0;
 }
 
+while (src[i] != '\0' && src[i] != '\n')
+{
+dest[j++] = src[i++];
+}
+
+dest[j] = '\0';
+}
+
+/**
+* strmix - a function to copy to strings and concatenate if
+*	   the destination have a string already.
+* @src: the string to be copied
+* @dest: the destination of the copied string
+*
+* Return: returns nothing (void)
+*/
+
+void strmix(char *src, char *dest)
+{
+int i = 0;
+int j;
+
+if (!dest)
+{
+j = 0;
+}
+else
+{
+j = strleng(dest);
+}
+
+while (src[i] != '\0' && src[i] != '\n')
+{
+dest[j++] = src[i++];
+}
+
+dest[j] = '\0';
+}
+
+/**
+* strbrk - a function that breaks a long string in to arrauy of strings
+* @line: the string to be breakdown
+* @c: a chacter specifier where to break the string
+*
+* Return: returns the array of the words.
+*/
+
+char **strbrk(char *line, char c)
+{
+int i = 0, j = 0;
+char **array, **tmp;
+
+if (!line || line[0] == '\0')
+{
+return (NULL);
+}
+array = malloc(sizeof(*array) * 128);
+if (array == NULL)
+{
+perror("Couldn't Allocate");
+return (NULL); }
+while (line[i] != '\0')
+{
+if (j > 124)
+{
+tmp = realloc(array, sizeof(*array) * (i + 4));
+if (tmp == NULL)
+{
+perror("Couldn't Reallocate");
+return (NULL); }
+else
+{
+array = tmp; }
+}
+if (line[i] == c)
+{
+i++;
+continue; }
+array[j] = stringer(line, i, array[j], c);
+if (array[j] == NULL)
+{
+return (NULL); }
+i += strleng(array[j++]);
+}
+array[j] = NULL;
+return (array);
+}
 
 /**
 * strleng - measures a length of a string
@@ -109,41 +163,12 @@ void strmix(char *src, char *dest)
 
 int strleng(char *s)
 {
-        int len = 0;
-        while (s[len] != '\0')
-        {
-                len++;
-        }
-        return (len);
+int len = 0;
+
+while (s[len] != '\0')
+{
+len++;
 }
 
-
-/**
-* strcomp - a function to compare two strings
-* @str: the first string to compare
-* @equ: the secodn string to comapre too
-*
-* Return: returns an integer indicater of the boolean
-*/
-int strcomp(char *str, char *equ)
-{
-        int i = 0;
-        int result = 0;
-        if (str == NULL || equ == NULL)
-        {
-                return (0);
-        }
-        while (equ[i] != '\0' || str[i] != '\0')
-        {
-                if (str[i] == equ[i])
-                {
-                        result = 1;
-                }
-                else
-                {
-                        result = 0;
-                }
-                i++;
-        }
-        return (result);
+return (len);
 }
