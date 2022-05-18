26483acc64ae4b929e63872332ea49a43a5c7526
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-05-18 20:53:30 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -11,6 +11,7 @@
 #include <signal.h>
 
 extern int errno;
+extern char **environ
 /**
 * struct selectfunction - a struct array of functions
 * @command: a pointer to the caharacter of the command string
