d98dfb3f9ecba6322cbb827899eb9ba13da6822b
fellydyll <yeboahfelix022@gmail.com>
2022-05-18 01:45:38 -0700

    Update stringfuc.c

--- stringfuc.c
+++ stringfuc.c
@@ -9,30 +9,30 @@
 
 int getstr(char *str)
 {
-        char **line;
-        size_t siz = 0;
+	char **line;
+	size_t siz = 0;
 
-        signal(SIGINT, sigintHandler);
-        line = malloc(24);
-        if (!line)
-        {
-                return (-1);
-        }
+	signal(SIGINT, sigintHandler);
+	line = malloc(24);
+	if (!line)
+	{
+		return (-1);
+	}
 
-        if ((getline(line, &siz, stdin)) == -1)
-        {
-                return (-1);
-        }
+	if ((getline(line, &siz, stdin)) == -1)
+	{
+		return (-1);
+	}
 
-        line[0][siz] = '\0';
+	line[0][siz] = '\0';
 
-        strcopy(line[0], str);
+	strcopy(line[0], str);
 
-        if (line[0][siz - 2] == '\\')
-        {
-                do {
-                        str[strleng(str) - 1] = ' ';
-                        siz = 0;
+	if (line[0][siz - 2] == '\\')
+	{
+		do {
+			str[strleng(str) - 1] = ' ';
+			siz = 0;
                         printprompt(1);
 
                         if ((getline(line, &siz, stdin)) == -1)
