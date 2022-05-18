b3df3fe80d698ded21222338fe67730c05c4e089
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-05-18 20:57:51 +0100

    Update shellprocessor.c

--- shellprocessor.c
+++ shellprocessor.c
@@ -26,3 +26,43 @@ exit(i);
 *
 * Return: returns 0 in success and -1 if it fails
 */
+
+int changedire(char **line)
+{
+if (line[1] == NULL)
+{
+chdir(getenv("HOME"));
+}
+else if (chdir(line[1]) == -1)
+{
+write(1, line[0], strleng(line[0]));
+write(1, ": ", 3);
+write(1, ": cd", strleng(": cd"));
+write(1, ": can't cd to ", strleng(": can't cd to "));
+write(1, line[1], strleng(line[1]));
+write(1, "\n", 2);
+return (-1);
+}
+
+free(line);
+return (1);
+}
+
+/**
+* printprompt - a function to display the enter command character
+* @i: an integer value to indicate which icon to display
+*
+* Return: void
+*/
+
+void printprompt(int i)
+{
+if (i == 0)
+{
+write(1, "USH$ ", strleng("USH$ "));
+}
+else if (i == 1)
+{
+write(1, "ush> ", strleng("ush> "));
+}
+}
